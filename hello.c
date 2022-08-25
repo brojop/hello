e5853ba4491182bbec5bc57b8dd1d8cc88eaff45
Jordyn Cheyney <jcheyney2l@huskers.unl.edu>
2022-08-25 20:39:41 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -7,6 +7,8 @@ int main( int argc, char **argv) {
     printf("Hello, I'm Brandon Rojop!\n");
     printf("Major is Computer Engineering!\n");
     printf("Minor is none!\n");
-
+    printf("Hello, I'm Jordyn Cheyney!\n");
+    printf("Major is Computer Science!\n");
+    
     return 0;
 }
