fddf9e72c9ab3465dc6ea0d7c1997a596c89768f
brojop <111544253+brojop@users.noreply.github.com>
2022-11-14 23:51:22 -0600

    Add files via upload

---
+++ airportTester.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "airport.h"
+
+int main(int argc, char **argv) {
+
+    Airport* createAirport(const char* ,
+                            const char* commercial services,
+                            const char* Chicago Midway International Airport,
+                            double 41.609830894,
+                            double -87.407665036,
+                            int 597,
+                            const char* Chicago,
+                            const char *NA);
+
+
+
+
+    Airport* createAirport(const char* ,
+                            const char* commercial services,
+                            const char* Cairo International Airport,
+                            double 30.119832854,
+                            double 31.40333172,
+                            int 467,
+                            const char* Cairo,
+                            const char* EG);
+
+
+
+    Airport* createAirport(const char* ,
+                            const char* commercial services,
+                            const char* London City Airport,
+                            double 43.0285506,
+                            double -81.149864,
+                            int 274,
+                            const char* London,
+                            const char *GB);
+
+
+
+
+
+    return 0;
+}
